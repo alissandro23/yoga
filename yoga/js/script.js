85b2b49e7bdc0eda56cae7df1a8be1cf9b238ba7
Alessandro <alessandrokr592@gmail.com>
2023-07-30 16:16:13 +0200

    timer functionality

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -26,3 +26,35 @@ tabs.forEach(function (tab) {
 
 showTab(tabs[0])
 
+class Timer {
+    endTime;
+    #timerElement;
+
+    constructor(endTime, timerSelector) {
+       this.endTime = endTime
+       this.#timerElement = document.querySelector(timerSelector)
+    }
+
+    run() {
+        this.rerender()
+        setInterval(this.rerender.bind(this), 1000)
+    }
+
+    rerender() {
+        let timeDelta = this.endTime - new Date()
+        const hours = parseInt(timeDelta / 3600000)  
+        timeDelta -= hours * 3600000
+
+        const minutes = parseInt(timeDelta / 60000)  
+        timeDelta -= minutes * 60000
+
+        const seconds = parseInt(timeDelta / 1000) 
+        
+        this.#timerElement.querySelector(".hours").innerText = hours
+        this.#timerElement.querySelector(".minutes").innerText = minutes
+        this.#timerElement.querySelector(".seconds").innerText = seconds
+    }
+} 
+
+const timer = new Timer(new Date(1690811563549), '#timer')
+timer.run()
